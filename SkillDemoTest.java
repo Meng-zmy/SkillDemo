ac3ff7a520332d3ac7d37b37fbf3e7f8e143c5c5
Meng-zmy <mez001@ucsd.edu>
2022-04-29 10:11:32 -0700

    Create SkillDemoTest.java

---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void testFullName(){
+        assertEquals("Zhang Mengyuan", SkillDemo.fullName("Mengyuan", "Zhang"));
+        // This test will not pass
+    }
+}
